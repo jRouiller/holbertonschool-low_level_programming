367fae1aea4c7102950ef1069726e40cb4de81fd
jRouiller <janisfnune@gmail.com>
2023-02-15 10:45:09 -0800

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/*
- * This function should print the text bellow and a newline
-*/
-
 /**
  * main - Entry point
  *
@@ -12,7 +8,7 @@
 
 int main(void)
 {
-	puts("Size of a char: 1 byte(s)
+	printf("Size of a char: 1 byte(s)
 Size of an int: 4 byte(s)
 Size of a long int: 4 byte(s)
 Size of a long long int: 8 byte(s)
@@ -22,6 +18,6 @@ Size of a char: 1 byte(s)
 Size of an int: 4 byte(s)
 Size of a long int: 8 byte(s)
 Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)")
+Size of a float: 4 byte(s)\n")
 	return (0)
 }
