da6560a892a74b40bfd63912b226e5d295c16074
jRouiller <janisfnune@gmail.com>
2023-02-15 10:43:37 -0800

    task6

---
+++ hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * This function should print the text bellow and a newline
+*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+julien@ubuntu:~/c/$ ./size64
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 8 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)")
+	return (0)
+}
