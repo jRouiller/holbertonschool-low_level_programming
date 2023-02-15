a5684a57459b76ec5b99b9318faa820d82bb7ade
jRouiller <janisfnune@gmail.com>
2023-02-15 15:04:55 -0800

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,16 +8,7 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-julien@ubuntu:~/c/$ ./size64
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)\n")
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\njulien@ubuntu:~/c/$ ./size64
+Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n")
 	return (0)
 }
