d9443654deba9c113bccc0d41b2c6639ab1db403
jRouiller <janisfnune@gmail.com>
2023-02-19 09:07:30 -0800

    Starting with control statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,13 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf("%d\n is negative", n);
+		printf("%d is negative\n", n);
 	} else if (n > 0)
 	{
-		printf("%d\n is positive", n);
+		printf("%d is positive\n", n);
 	} else
 	{
-		printf("%d\n is zero", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
