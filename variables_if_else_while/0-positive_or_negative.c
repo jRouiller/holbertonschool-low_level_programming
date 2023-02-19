11de715eea18884d9a9d2d3605ab9301a91d4d81
jRouiller <janisfnune@gmail.com>
2023-02-19 08:58:33 -0800

    Starting with control statements

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)     
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(NULL));
+
+	n = rand();
+
+
+	if (n < 0)
+	{
+		printf("%d\n, is negative", n)
+	} else (n > 0)
+	{
+		printf("%d\n, is positive", n)
+	} else
+	{
+		printf("%d\n, is zero", n)
+	}
+
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Welcome to my new directory, here I will attend lessons of variables in If, For and While/While Do control statements.
