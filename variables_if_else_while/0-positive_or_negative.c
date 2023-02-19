ae465405eca82baace1a964547886a51d13c8dcc
jRouiller <janisfnune@gmail.com>
2023-02-19 10:07:04 -0800

    updating

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,17 @@ int main(void)
 
 	n = rand() % 201 - 100;
 
-
 	if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	} else if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is zero\n", n);
 	} else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is negative\n", n);
 	}
+
 
 	return (0);
 }
