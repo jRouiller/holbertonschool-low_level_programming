0d977213fda7edee1a4bdcaa95c57e915fbd4465
jRouiller <janisfnune@gmail.com>
2023-02-26 09:46:44 -0800

    task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,20 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * _isupper - main
  *
- * Return: Always 0.
+ * Return: return
  */
 
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,13 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <unistd.h>
 
-int_isupper(int c);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
