c7e91e93a71fe44c1d8f6f8ca873331efa8891bb
jRouiller <janisfnune@gmail.com>
2023-02-26 10:28:51 -0800

    task3

---
+++ more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *print_numbers - main
+ *   *Return: a
+ *    *
+ *     *
+ *      */
+void print_numbers(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
