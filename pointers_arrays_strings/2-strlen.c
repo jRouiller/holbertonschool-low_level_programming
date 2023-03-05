284ba7f1a0ac0ab8e67beef1da210335aa7e656c
jRouiller <janisfnune@gmail.com>
2023-03-05 10:40:01 -0800

    task3

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *  *_strlen - main func
- *   *@s: string
- *    *Return: string lenght
- *     */
+ * _strlen - main func
+ * @s: string
+ * Return: string lenght
+ */
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 	{
 		len++;
