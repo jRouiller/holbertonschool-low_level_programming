0979fcda64eeeb904b681b4f8e7aca7034de30be
jRouiller <janisfnunez@gmail.com>
2023-06-21 11:33:20 -0700

    uploading all task for correcting

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - main
+ * @a: a
+ * @b: b
+ * Return: return
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  *more_numbers - main
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int i, b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
